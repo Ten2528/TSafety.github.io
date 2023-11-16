8194fd872a4cc3bb2378041f198d29b8759a5038
Ten2528 <146884742+Ten2528@users.noreply.github.com>
2023-11-16 09:33:31 -0500

    main

--- Parte de angel/Download.js
+++ Parte de angel/Download.js
@@ -1,35 +1,33 @@
 const downloadBtn = document.querySelector(".download-btn");
-//Este es el link de descarga de google drive 
+//Este es el link de descarga de google drive
 const fileLink = "https://drive.google.com/uc?export=download&id=1h5hNCZWpuM2ZribRuAYASj5v6afh7oif";
 
 const initTimer = () => {
-    //Por lo que tengo entendido si downloadBtn contiene disable-timer entonces solo se ejecutura la condicional?
-    if (downloadBtn.classList.contains("disable-timer")) {
-        return (location.href = fileLink);
+    //Por lo que tengo entendido si downloadBtn contiene disable-timer entonces solo se ejecutara la condicional?
+    if(downloadBtn.classList.contains("disable-timer")) {
+        return location.href = fileLink;
     }
     //El atributo data timer de html
     let timer = downloadBtn.dataset.timer;
     downloadBtn.classList.add("timer");
-    //Esto es para reemplazar el downloadbtn del html
-    downloadBtn.innerHTML = `Your file will download in <b>${timer}</b> seconds`;
+    //Esto es para reemplazar el downloadbtn del html 
+    downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
 
-    // Aquí Creamos la variable initcounter con set interval
+    //Aquí creamos la variable initcounter con set Interval 
     const initCounter = setInterval(() => {
-        if(timer > 0){
-        timer--;
-        return (downloadBtn.innerHTML `Your file will download in  <b>${timer}</b>seconds`); 
+        if(timer > 0) {
+            timer--;
+            return downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
         }
         clearInterval(initCounter);
         location.href = fileLink;
-        downloadBtn.textContent ="Your file is downloading...";
-
+        downloadBtn.innerText = "Your file is downloading...";
         setTimeout(() => {
-            downloadBtn.classList.replace("timer","disable-timer");
-            downloadBtn.innerHTML =`<span class=" icon material-symbols-outlined">vertical_align_bottom</span>
-            <span class="text">Download again </span>`;
-        },3000);
-    },1000); //1000 milliseconds=1seg
-};
-
+            downloadBtn.classList.replace("timer", "disable-timer");
+            downloadBtn.innerHTML = `<span class="icon material-symbols-rounded">vertical_align_bottom</span>
+                                     <span class="text">Download Again</span>`;
+        }, 3000);
+    }, 1000);
+}
 
 downloadBtn.addEventListener("click", initTimer);
